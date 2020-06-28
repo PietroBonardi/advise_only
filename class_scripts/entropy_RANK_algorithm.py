e531bcdb66910d9b56af2650c28975b49b4cb8ba
Willinki <davide.badalotti@outlook.it>
2020-06-28 20:11:39 +0200

    renamed files for more explicative names

--- class_scripts/entropy_RANK_algorithm.py
+++ class_scripts/entropy_RANK_algorithm.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import numpy as np 
 import math
-from entropy_selection_class import dataframe_ext
+from dataset_extended_class import dataframe_ext
 ##############################################################################
 #                                                                            #
 #                   RANK ALGORITHM                                           #
